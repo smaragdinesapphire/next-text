81702d63010353dde862b26add6c480d2bb7a679
Jie.Zhang <smaragdinesapphire@gmail.com>
2021-06-12 13:57:34 +0800

    test: test install at tabletop

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,7 @@ import useTranslation from 'next-translate/useTranslation';
 import prepareDB from '../mySrc/utils/prepareDB';
 import ManagerContext, { managers } from '../mySrc/contexts/ManagerContext';
 import '../styles/globals.scss';
+import { Button } from '@material-ui/core';
 
 interface HandleContextMenu {
   (e: MouseEvent): void;
@@ -25,15 +26,65 @@ const Container = styled.div`
 
 const theme = createMuiTheme(themeConfig);
 
+/**
+ * The BeforeInstallPromptEvent is fired at the Window.onbeforeinstallprompt handler
+ * before a user is prompted to "install" a web site to a home screen on mobile.
+ *
+ * @deprecated Only supported on Chrome and Android Webview.
+ */
+interface BeforeInstallPromptEvent extends Event {
+  /**
+   * Returns an array of DOMString items containing the platforms on which the event was dispatched.
+   * This is provided for user agents that want to present a choice of versions to the user such as,
+   * for example, "web" or "play" which would allow the user to chose between a web version or
+   * an Android version.
+   */
+  readonly platforms: Array<string>;
+
+  /**
+   * Returns a Promise that resolves to a DOMString containing either "accepted" or "dismissed".
+   */
+  readonly userChoice: Promise<{
+    outcome: 'accepted' | 'dismissed';
+    platform: string;
+  }>;
+
+  /**
+   * Allows a developer to show the install prompt at a time of their own choosing.
+   * This method returns a Promise.
+   */
+  prompt(): Promise<void>;
+}
+
+function openCreatePostModal(installEvent: BeforeInstallPromptEvent, setInstallEvent): void {
+  if (installEvent) {
+    installEvent.prompt();
+    installEvent.userChoice.then(function (choiceResult) {
+      console.log(choiceResult.outcome);
+      if (choiceResult.outcome == 'dismissed') console.log('使用者取消安裝');
+      else console.log('使用者安裝');
+    });
+    setInstallEvent(null);
+  }
+}
+
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   const [DBLoading, setDBLoading] = useState(true);
   const [showDBOpenFail, setShowDBOpenFail] = useState(false);
   const [showNoSupportDB, setShowNoSupportDB] = useState(false);
+  const [installEvent, setInstallEvent] = useState(null);
 
   useEffect(() => {
     const handleContextMenu: HandleContextMenu = e => e.preventDefault();
     window.addEventListener('contextmenu', handleContextMenu);
     autoRestBaseUnit.start();
+    window.addEventListener('beforeinstallprompt', event => {
+      // Prevent Chrome <= 67 from automatically showing the prompt
+      event.preventDefault();
+      // Stash the event so it can be triggered later.
+      setInstallEvent(event);
+      return false;
+    });
     return () => {
       autoRestBaseUnit.stop();
       window.removeEventListener('contextmenu', handleContextMenu);
@@ -72,6 +123,7 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
           <ManagerContext.Provider value={managers}>
             <CssBaseline>
               <Container>
+                <Button onClick={() => openCreatePostModal(installEvent, setInstallEvent)}>Click</Button>
                 <Component {...pageProps} DBLoading={DBLoading} />
               </Container>
               <Alert
